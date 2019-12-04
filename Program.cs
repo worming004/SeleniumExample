7cb73e1adaa8734923d1cb4a26ab34e8f0fd6bdb
worming004 <worming@hotmail.be>
2019-12-04 17:46:37 +0100

    example

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace ReachSelenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = // Put url here with port and /wd/hub path
+            var driver = new RemoteWebDriver(new Uri(url), (new OpenQA.Selenium.Chrome.ChromeOptions()).ToCapabilities(), TimeSpan.FromSeconds(600));
+
+            try
+            {
+                PingGoogle(driver);
+            }
+            finally
+            {
+                driver.Close();
+            }
+        }
+
+        static void PingGoogle(IWebDriver driver)
+        {
+            driver.Navigate()
+            .GoToUrl("https://www.google.com");
+
+            Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            ss.SaveAsFile(Path.Combine("./", DateTime.Now.ToLongDateString() + ".jpg"));
+        }
+    }
+}
